e87fe3dfe00fb66596425b1e8b7f336508e88ba5
edithpwa <74992097+edithpwa@users.noreply.github.com>
2020-11-24 17:42:33 -0600

    Create funcionalidad.js

---
+++ funcionalidad.js
@@ -0,0 +1,42 @@
+function operaciones(op)
+{
+
+    var ops = {
+      
+
+       densidad: function dividirNumeros(n1, n2) {
+            return (parseInt(n1) / parseInt(n2));
+        }
+
+
+    };
+
+
+
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+    
+    //Comprobamos si se ha introducido números en las cajas
+    if (isNaN(parseFloat(document.getElementById('num1').value))) {
+        document.getElementById('resultado').innerHTML="<span style='color: red;'> Ingrese el primer numero</span>";
+        document.getElementById("num1").innerText = "0";
+        document.getElementById("num1").focus();
+        } else if (isNaN(parseFloat(document.getElementById('num2').value))) {
+        document.getElementById('resultado').innerHTML="<span style='color: red;'> Ingrese el segundo numero</span>";
+        document.getElementById("num2").innerText = "0";
+        document.getElementById("num2").focus();
+    }
+    else {
+    //Si se han introducido los números en ámbas cajas, operamos:
+        switch(op) {
+            
+            case 'densidad':
+                var resultado = ops.densidad(num1, num2);
+                document.getElementById('resultado').innerHTML="<span style='color: green;'>"+resultado+"</span>";
+                break;
+
+        }
+    }
+
+}
